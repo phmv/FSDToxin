ddcb4889725f778fc182aa93349af7d397888155
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-04-11 23:51:15 +0300

    header authorization block hiding and opening with profile button is added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,3 +3,24 @@ import "../nav-menu/nav-menu";
 import "../link-button/link-button";
 
 import "./header.scss";
+
+document.querySelectorAll(".header").forEach((header) => {
+  let authorizationBlock = header.querySelector(".header__authorization");
+  let profileButton = header.querySelector(".header__profile-button");
+  let authorizationBlockIsOpened = false;
+  let authorizationBlockOpenedClass = "header__authorization--opened";
+  profileButton.addEventListener("click", profileButtonClickHandler);
+  document.addEventListener("click", outOfAuthorizationBlockClickHandler);
+
+  function profileButtonClickHandler(e) {
+    authorizationBlock.classList.toggle(authorizationBlockOpenedClass);
+    authorizationBlockIsOpened = !authorizationBlockIsOpened;
+  }
+
+  function outOfAuthorizationBlockClickHandler(e) {
+    if (authorizationBlockIsOpened && !profileButton.contains(e.target) && !authorizationBlock.contains(e.target)) {
+      authorizationBlock.classList.remove(authorizationBlockOpenedClass);
+      authorizationBlockIsOpened = false;
+    }
+  }
+});
--- src/components/nav-menu/nav-menu.js
+++ src/components/nav-menu/nav-menu.js
@@ -1,1 +1,12 @@
 import "./nav-menu.scss";
+
+// document.querySelectorAll(".nav-menu__dropdown").forEach((dropdown) => {
+//   let dropdownTitle = dropdown.querySelector(".nav-menu__dropdown-title");
+//   let dropdownContent = dropdown.querySelector(".nav-menu__dropdown-list");
+
+//   dropdownTitle.addEventListener("click", navMenuDropdownClickHandler.bind(dropdownContent));
+
+//   function navMenuDropdownClickHandler() {
+//     this.classList.toggle("nav-menu__dropdown-list--active");
+//   }
+// });
