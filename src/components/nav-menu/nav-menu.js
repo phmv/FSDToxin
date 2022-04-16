5b855ebdf7f3293cfda3987254e61dc2595bb9f0
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-04-17 01:04:25 +0300

    Adaptive header is done

--- src/components/nav-menu/nav-menu.js
+++ src/components/nav-menu/nav-menu.js
@@ -4,17 +4,20 @@ document.querySelectorAll(".nav-menu").forEach((navMenu) => {
   let navMenuList = navMenu.querySelector(".nav-menu__list");
   let navMenuButton = navMenu.querySelector(".nav-menu__burger");
   let navMenuDropdowns = navMenu.querySelectorAll(".nav-menu__dropdown");
+  let burgerOpenedClass = "nav-menu__burger--opened";
   let navMenuListOpenedClass = "nav-menu__list--opened";
   navMenuButton.addEventListener("click", navMenuButtonClickHandler);
   document.addEventListener("click", outOfNavMenuClickHandler);
 
   function navMenuButtonClickHandler(e) {
     navMenuList.classList.toggle(navMenuListOpenedClass);
+    navMenuButton.classList.toggle(burgerOpenedClass);
   }
 
   function outOfNavMenuClickHandler(e) {
     if (!navMenuList.contains(e.target) && !navMenuButton.contains(e.target)) {
       navMenuList.classList.remove(navMenuListOpenedClass);
+      navMenuButton.classList.remove(burgerOpenedClass);
     }
   }
 
