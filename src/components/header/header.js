56223c80fd24fa7aa8ba033bf206e9d7e796ff1f
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-04-24 05:39:51 +0300

    Media query transition blink fixed.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,8 @@ import "../link-button/link-button";
 
 import "./header.scss";
 
+import * as transition from "../../utils/transition-block/transition-blocker.js";
+
 document.querySelectorAll(".header").forEach((header) => {
   let authorizationBlock = header.querySelector(".header__authorization");
   let profileButton = header.querySelector(".header__profile-button");
@@ -12,13 +14,17 @@ document.querySelectorAll(".header").forEach((header) => {
   profileButton.addEventListener("click", profileButtonClickHandler);
   document.addEventListener("click", outOfAuthorizationBlockClickHandler);
 
+  transition.block(authorizationBlock);
+
   function profileButtonClickHandler(e) {
+    transition.once(authorizationBlock);
     authorizationBlock.classList.toggle(authorizationBlockOpenedClass);
     authorizationBlockIsOpened = !authorizationBlockIsOpened;
   }
 
   function outOfAuthorizationBlockClickHandler(e) {
     if (authorizationBlockIsOpened && !profileButton.contains(e.target) && !authorizationBlock.contains(e.target)) {
+      transition.once(authorizationBlock);
       authorizationBlock.classList.remove(authorizationBlockOpenedClass);
       authorizationBlockIsOpened = false;
     }
