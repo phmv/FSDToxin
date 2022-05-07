3f8757f1d463c5505a814198edd38688bce1fa4d
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-05-07 23:13:45 +0300

    Nav menu for mobile and tablets is done

--- src/components/nav-menu/nav-menu.js
+++ src/components/nav-menu/nav-menu.js
@@ -4,6 +4,7 @@ import * as transition from "../../utils/transition-block/transition-blocker.js"
 
 document.querySelectorAll(".nav-menu").forEach((navMenu) => {
   let list = navMenu.querySelector(".nav-menu__list");
+  let contentCover = navMenu.querySelector(".nav-menu__content-cover");
   let burgerButton = navMenu.querySelector(".nav-menu__burger");
   let dropdowns = navMenu.querySelectorAll(".nav-menu__dropdown");
   let isNavMenuOpened = false;
@@ -17,8 +18,10 @@ document.querySelectorAll(".nav-menu").forEach((navMenu) => {
   });
 
   transition.block(list);
+  transition.block(contentCover);
 
   function burgerButtonClickHandler(e) {
+    transition.once(contentCover);
     transition.once(list);
     isNavMenuOpened = !isNavMenuOpened;
     navMenu.dataset.active = isNavMenuOpened;
@@ -26,6 +29,7 @@ document.querySelectorAll(".nav-menu").forEach((navMenu) => {
 
   function outOfNavMenuClickHandler(e) {
     if (!list.contains(e.target) && !burgerButton.contains(e.target) && isNavMenuOpened) {
+      transition.once(contentCover);
       transition.once(list);
       isNavMenuOpened = false;
       navMenu.dataset.active = isNavMenuOpened;
@@ -33,18 +37,24 @@ document.querySelectorAll(".nav-menu").forEach((navMenu) => {
   }
 
   dropdowns.forEach((dropdown) => {
+    let dropdownList = dropdown.querySelector(".nav-menu__dropdown-list");
     let navMenuDropdownOpenedClass = "nav-menu__dropdown--opened";
-
     dropdown.addEventListener("click", dropdownClickEventHandler);
     document.addEventListener("click", outOfNavMenuDropdownClickHandler);
 
     function dropdownClickEventHandler(e) {
       dropdown.classList.toggle(navMenuDropdownOpenedClass);
+      if (dropdownList.style.maxHeight) {
+        dropdownList.style.maxHeight = null;
+      } else {
+        dropdownList.style.maxHeight = dropdownList.scrollHeight + "px";
+      }
     }
 
     function outOfNavMenuDropdownClickHandler(e) {
       if (!dropdown.contains(e.target)) {
         dropdown.classList.remove(navMenuDropdownOpenedClass);
+        dropdownList.style.maxHeight = null;
       }
     }
   });
