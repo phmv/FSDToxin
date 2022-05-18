40b2d4011fc043723a7fe65fccecacd1106bf69a
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-05-19 02:51:23 +0300

    Pug mixin description updated. Expandable checkbox animation reworked. No more whitespace cuz of scaleY

--- src/components/checkbox-list/checkbox-list.js
+++ src/components/checkbox-list/checkbox-list.js
@@ -1,9 +1,21 @@
-import '../checkbox/checkbox'
-import '../toggle-button/toggle-button'
-import './checkbox-list.scss'
+import "../checkbox/checkbox";
+import "../toggle-button/toggle-button";
+import { transitionEndEvent } from "../../utils/transition-block/transition-blocker";
+import "./checkbox-list.scss";
 
-$(document).ready(function(){
-  $('.checkbox-list--expandable').children('.checkbox-list__title').on('click', function() {
-    $(this).parent().toggleClass('checkbox-list--active')
-  });
-})
+$(document).ready(function () {
+  let $this = $(".checkbox-list--expandable");
+  let title = $this.find(".checkbox-list__title");
+  let container = $this.find(".checkbox-list__container");
+
+  title.on("click", titleClickEventHandler);
+
+  function titleClickEventHandler(e) {
+    if (container.css("max-height") !== "0px") {
+      container.css("max-height", "");
+    } else {
+      container.css("max-height", `${container.prop("scrollHeight")}px`);
+    }
+    $this.toggleClass("checkbox-list--active");
+  }
+});
