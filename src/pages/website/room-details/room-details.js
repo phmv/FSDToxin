0098fc814adbd9750febd54242d5fdcbcab18640
Evgeniy Pakhomov <pakhomov.eval@gmail.com>
2022-06-02 23:07:08 +0300

    Room details page is done

---
+++ src/pages/website/room-details/room-details.js
@@ -0,0 +1,8 @@
+import "../../../templates/website-page/website-page";
+import "../../../components/carousel/carousel";
+import "../../../components/features-list/features-list";
+import "../../../components/guest-review/guest-review";
+import "../../../components/bullet-list/bullet-list";
+import "../../../components/donut-chart/donut-chart";
+import "../../../forms/room-booking-card/room-booking-card";
+import "./room-details.scss";
